dcdb1cd4c2d38e0f97cc472eac592aa5ee9c0d29
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-11-03 22:53:21 +0100

    finishing up tanstack router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,19 +1,15 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
-    <div className='w-full h-full'>
+    <div className="w-full h-full">
       <div>
-      <Link to="/" >
-            Home
-          </Link>
-        <Link to="/about" >
-            About
-          </Link>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
       </div>
       <Outlet />
       <TanStackRouterDevtools />
     </div>
   ),
-})
+});
