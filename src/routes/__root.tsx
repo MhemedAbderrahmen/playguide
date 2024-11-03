374e2a4e7e70effeb99045a1c2f7d14ba849ffca
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-11-03 23:07:16 +0100

    refactored navigation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,13 @@
-import { ModeToggle } from '@/components/mode-toggle';
+import { Navigation } from '@/components/navigation';
 import { ThemeProvider } from '@/components/theme-provider';
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="w-full h-full p-8">
-        <div className='flex flex-row gap-2'>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <ModeToggle />
-        </div>
+        <Navigation />
         <Outlet />
         <TanStackRouterDevtools />
       </div>
