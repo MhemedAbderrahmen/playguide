d8c73aa93f702901b893d57a794972186c2c8f30
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-11-03 22:45:36 +0100

    adding shadcn and tanstack react router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,19 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+export const Route = createRootRoute({
+  component: () => (
+    <div className='w-full h-full'>
+      <div>
+      <Link to="/" >
+            Home
+          </Link>
+        <Link to="/about" >
+            About
+          </Link>
+      </div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  ),
+})
