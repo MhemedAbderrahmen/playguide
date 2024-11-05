0429acf07e30e3118a8bd957dea8919de641ff83
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-11-05 20:50:21 +0100

    adding custom useTitle Hook

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,9 +1,11 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { useTitle } from '@/hooks/use-title';
+import { createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/about')({
-  component: RouteComponent,
-})
+  component: About,
+});
 
-function RouteComponent() {
-  return 'Hello /about!'
+function About() {
+  useTitle('About');
+  return <div>About</div>;
 }
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,16 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { useTitle } from '@/hooks/use-title';
+import { createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
-})
+});
 
 function Index() {
+  useTitle('Home');
+
   return (
     <div>
       <h3>Welcome Home!</h3>
     </div>
-  )
+  );
 }
